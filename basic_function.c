87ddb1d22e9c11550ab33c714ae33b73d6f5c51e
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-03-29 09:55:47 +0530

    added function to_fahrenheit and to_celsius and modified gcd

--- basic_function.c
+++ basic_function.c
@@ -14,16 +14,15 @@ return num*num*num;
 }
 
 int GCD(int num1,int num2){
-  while(num2 == 0){
+  while(num2 != 0){
     int rem = num1%num2;
-    int quotient = num1/num2;
+    num1 = num2;
     num2 = rem;
-    num1 = quotient;
   };
   return num1;
 }
 
-int HCF(int num1,int num2){
+int LCM(int num1,int num2){
   return (num1*num2)/GCD(num1,num2);
 }
 
@@ -33,6 +32,14 @@ double SI(int p, int r, int t){
 
 double CI(float p, float r, float t){
   return p*(pow(1+r/100,t))-p;
+}
+
+double to_fahrenheit(double temp_in_c){
+  return ((9/5.0)*(temp_in_c))+32;
+}
+
+double to_celsius(double temp_in_f){
+  return (5/9.0)*(temp_in_f-32);
 }
 
 int main(void){
@@ -59,16 +66,26 @@ int main(void){
   scanf("%d %d",&num, &another_num);
   printf("GCD of %d and %d is : %d.\n",num,another_num,GCD(num,another_num));
 
-  printf("\nEnter two number to find HCF: \n");
+  printf("\nEnter two number to find LCM: \n");
   scanf("%d %d",&num, &another_num);
-  printf("HCF of %d and %d is : %d.\n",num,another_num,HCF(num,another_num));
+  printf("LCM of %d and %d is : %d.\n",num,another_num,LCM(num,another_num));
 
   float principal, rate, time;
   printf("\nEnter principal, rate and interest to find SI.\n");
   scanf("%f %f %f",&principal, &rate, &time);
   printf("SI of number is : %0.2f.\n",SI(principal,rate,time));
-   printf("\nEnter principal, rate and interest to find CI.\n");
+
+  printf("\nEnter principal, rate and interest to find CI.\n");
   scanf("%f %f %f",&principal, &rate, &time);
   printf("CI of number is : %0.2f.\n",CI(principal,rate,time));
+
+  float temp;
+  printf("\nEnter temperature in celsius to convert in fahrenheit.\n");
+  scanf("%f",&temp);
+  printf("Temp in fahrenheit is : %0.2f.\n",to_fahrenheit(temp));
+
+  printf("\nEnter temperature in fahrenheit to convert in celsius.\n");
+  scanf("%f",&temp);
+  printf("Temp in celsius is : %0.2f.\n",to_celsius(temp));
   return 0;
 }
