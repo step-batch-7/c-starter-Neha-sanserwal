9473ab2e778693da0200bd3748a5941897158d5d
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-03-29 09:13:10 +0530

    added function to calculate CI

--- basic_function.c
+++ basic_function.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <math.h>
 
 unsigned char is_even(num){
-  return num%2 == 0; 
+return num%2 == 0; 
 }
 
 int square(int num){
@@ -9,7 +10,7 @@ int square(int num){
 }
 
 int cube(int num){
-  return num*num*num;
+return num*num*num;
 }
 
 int GCD(int num1,int num2){
@@ -26,14 +27,18 @@ int HCF(int num1,int num2){
   return (num1*num2)/GCD(num1,num2);
 }
 
-int SI(int p, int r, int i){
-  return (p*r*i)/100;
+double SI(int p, int r, int t){
+  return (p*r*t)/100;
+}
+
+double CI(float p, float r, float t){
+  return p*(pow(1+r/100,t))-p;
 }
 
 int main(void){
 
   int num, another_num;
-  printf("Enter a number : ");
+  printf("Enter a number to find even or odd: ");
   scanf("%d",&num);
 
   if(is_even(num)){
@@ -42,21 +47,28 @@ int main(void){
     printf("%d is Odd Number.\n",num);
   }
 
+  printf("\nEnter a number to get square: ");
+  scanf("%d",&num);
   printf("Square of %d is %d.\n",num, square(num));
 
+  printf("\nEnter a number to get cube: ");
+  scanf("%d",&num);
   printf("Cube of %d is %d.\n",num, cube(num));
 
-  printf("Enter two number to find GCD: \n");
+  printf("\nEnter two numbers to find GCD: \n");
   scanf("%d %d",&num, &another_num);
-  printf("GCD of number is : %d.\n",GCD(num,another_num));
+  printf("GCD of %d and %d is : %d.\n",num,another_num,GCD(num,another_num));
 
-  printf("Enter two number to find HCF: \n");
+  printf("\nEnter two number to find HCF: \n");
   scanf("%d %d",&num, &another_num);
-  printf("HCF of number is : %d.\n",HCF(num,another_num));
+  printf("HCF of %d and %d is : %d.\n",num,another_num,HCF(num,another_num));
 
-  int principal, rate, interest;
-  printf("Enter principal, rate and interest to find SI.\n");
-  scanf("%d %d %d",&principal, &rate, &interest);
-  printf("SI of number is : %d.\n",SI(principal,rate,interest));
+  float principal, rate, time;
+  printf("\nEnter principal, rate and interest to find SI.\n");
+  scanf("%f %f %f",&principal, &rate, &time);
+  printf("SI of number is : %0.2f.\n",SI(principal,rate,time));
+   printf("\nEnter principal, rate and interest to find CI.\n");
+  scanf("%f %f %f",&principal, &rate, &time);
+  printf("CI of number is : %0.2f.\n",CI(principal,rate,time));
   return 0;
 }
--- square.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-  double num;
-  printf("Enter a number to find square : ");
-  scanf("%lf",&num);
-  printf("The square of %0.2lf is %0.2lf.",num,num*num);
-  return 0;
-}
